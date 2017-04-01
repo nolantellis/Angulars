19b9130e53d653d118fba97c261669d91664d1a1
Nolan Tellis <nolan.tellis@intelematics.com>
2017-04-01 20:33:54 +1100

    innit commit

---
+++ src/main/webapp/js/controller/ClockController.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+function ClockController($scope) {
+
+	var updateClock = function() {
+		$scope.clock = new Date();
+	};
+	
+	setInterval(function() {
+		$scope.$apply(updateClock);
+	}, 1000);
+	updateClock();
+
+}
